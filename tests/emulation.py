40067b8fd5d9934bc5325abed4c1d9b8d943c753
spacecheese <12695808+spacecheese@users.noreply.github.com>
2022-12-07 22:26:30 +0000

    Add some notes

--- tests/emulation.py
+++ tests/emulation.py
@@ -1,69 +1,11 @@
 from bluez_peripheral.util import *
-from typing import Tuple
 import subprocess
-import atexit
-
-BTVIRT_CMD = "btvirt"
-BTMON_CMD = "btmon"
-
-async def get_test_adapters(bus: MessageBus) -> Tuple[Adapter, Adapter]:
-    old_adapters = await Adapter.get_all(bus)
-    proc = subprocess.Popen([BTVIRT_CMD, "-l2"], stdout=subprocess.PIPE)
     
-    adapters = []
-    while len(adapters) < 2:
-        new_adapters = await Adapter.get_all(bus)
-        adapters = [i for i in new_adapters if i not in old_adapters]
-
-    server = adapters[0]
-    client = adapters[1]
-
-    await server.set_powered(True)
-    await client.set_powered(True)
-
-    # Make sure the virtual adapter process get cleaned up.
-    def cleanup():
-        proc.terminate()
-        proc.wait()
-    atexit.register(cleanup)
-
-    return (server, client)
-
-async def disconnect_test_adapters(server: Adapter, client: Adapter):
-    server_dev = None
-    for d in await client.get_devices():
-        if await d.get_address() == await server.get_address():
-            server_dev = d
-            break
-    if await server_dev.get_connected():
-        await server_dev.disconnect()
-
-async def pair_test_adapters(server: Adapter, client: Adapter):
-    # Prepare adapters for pairing.
-    await server.set_pairable(True)
-    await client.set_pairable(True)
-    await server.set_discoverable(True)
-    await client.set_discovering(True)
-
-    server_dev = None
-    for d in await client.get_devices():
-        if await d.get_address() == await server.get_address():
-            server_dev = d
-            break
-
-    if not await server_dev.get_paired():
-        print(await server_dev.get_paired())
-        await server_dev.pair()
-
-    if not await server_dev.get_trusted():
-        await server_dev.set_trusted(True)
-    
-
 class BTMonitor:
     _proc : subprocess.Popen = None
 
     def start(self):
-        self._proc = subprocess.Popen([BTMON_CMD], stdout=subprocess.PIPE)
+        self._proc = subprocess.Popen(["btmon"], stdout=subprocess.PIPE)
 
     def stop(self):
         self._proc.terminate()
@@ -75,6 +17,7 @@ class BTMonitor:
 #########################################################################
 import asyncio
 from bluez_peripheral.advert import Advertisement
+from bluez_peripheral.agent import *
 
 from bluez_peripheral.gatt.service import Service
 from bluez_peripheral.gatt.characteristic import characteristic, CharacteristicFlags as CharFlags
@@ -105,19 +48,72 @@ class HeartRateService(Service):
         self.heart_rate_measurement.changed(rate)
 
 async def main():
+    # TODO: Need a better solution than this.
+    #   Modalias property of adapter positivly identifies it as virtual (ie usb:v1D6Bp0246d0540 uniquely identifies the vendor and product id)
+
+    subprocess.Popen(["btvirt", "-l2"], stdout=subprocess.PIPE)
+
     bus = await get_message_bus()
     await bus.request_name("com.spacecheese.test")
 
-    server, client = await get_test_adapters(bus)
-    await pair_test_adapters(server, client)
+    # Need a better way of doing this
+    # Advertising manager takes a while to show up on new adapters
+    await asyncio.sleep(2)
+    
+    adapters = []
+    while len(adapters) < 2:
+        adapters = await Adapter.get_all(bus)
+
+    server = adapters[0]
+    client = adapters[1]
 
     service = HeartRateService()
-    await service.register(bus)
+    await service.register(bus, adapter = server)
 
-    #advert = Advertisement("Heart Monitor", ["180D"], 0x0340, 60)
-    #await advert.register(bus, ad[0])
+    async def agent_request(key):
+        print("Vaccuous Approval")
+        return True
+    def agent_cancel():
+        pass
 
-    await bus.wait_for_disconnect()
+    agent = YesNoAgent(agent_request, agent_cancel)
+    await agent.register(bus, True)
+
+    advert = Advertisement("Heart Monitor", ["180D"], 0x0340, 60)
+    await advert.register(bus, server)
+
+    await server.set_powered(True)
+    await client.set_powered(True)
+
+    try:
+        await server.set_pairable(True)
+        await client.set_pairable(True)
+        await server.set_discoverable(True)
+        await client.set_discovering(True)
+
+        server_dev = None
+        while server_dev is None:
+            for d in await client.get_devices():
+                if await d.get_address() == await server.get_address():
+                    server_dev = d
+                    break
+
+        if not await server_dev.get_paired():
+            await server_dev.pair()
+
+        while not await server_dev.get_paired():
+            pass
+
+        await server_dev.set_trusted(True)
+
+    except Exception as ex:
+        print("Encountered the following exception:")
+        print(ex)
+    finally:
+        print(f"Client Address = {await client.get_address()}, Server Address = {await server.get_address()}")
+        print("Sleeping")
+        while(1):
+            await asyncio.sleep(1)
 
 
 if __name__ == "__main__":
