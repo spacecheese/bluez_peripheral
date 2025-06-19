d133c00b668f0cafa1cad1ab07380785a60fe77d
Alex <32999995+alettsy@users.noreply.github.com>
2025-06-19 09:14:49 +0200

    Update conf.py
    Co-authored-by: Space Cheese <12695808+spacecheese@users.noreply.github.com>

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,7 +44,7 @@ napoleon_numpy_docstring = False
 # -- Intersphinx -------------------------------------------------------------
 intersphinx_mapping = {
     "python": ("https://docs.python.org/3", None),
-    "dbus_fast": ("https://python-dbus-next.readthedocs.io/en/latest/", None),
+    "dbus_fast": ("https://dbus-fast.readthedocs.io/en/latest/", None),
     # Add a backup inv to fix mapping of dbus_fast.aio.proxy_object.ProxyObject and dbus_fast.aio.message_bus.MessageBus
     "dbus_fast_alias": (os.path.abspath(os.path.dirname(__file__)), "dbus_fast.inv")
 }
