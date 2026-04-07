63b0c4bc556fd314e4ce43d84973b291ab64fb24
Space Cheese <12695808+spacecheese@users.noreply.github.com>
2026-04-07 14:28:19 +0000

    Adapter and Device fixes

--- bluez_peripheral/adapter.py
+++ bluez_peripheral/adapter.py
@@ -59,12 +59,13 @@ class Device:
 
     async def get_manufacturer_data(self) -> Dict[int, bytes]:
         """Returns the manufacturer data."""
-        return await self._device_interface.get_manufacturer_data()  # type: ignore
+        data = await self._device_interface.get_manufacturer_data()
+        return {k: v.value for k, v in data.items()} # type: ignore
 
-    async def get_service_data(self) -> List[Tuple[UUIDLike, bytes]]:
+    async def get_service_data(self) -> Dict[UUIDLike, bytes]:
         """Returns the service data."""
         data = await self._device_interface.get_service_data()  # type: ignore
-        return [(UUID16.parse_uuid(u), d) for u, d in data]
+        return {UUID16.parse_uuid(k): v.value for k, v in data.items()}
 
 
 class Adapter:
@@ -215,28 +216,37 @@ class Adapter:
         )
         object_manager_interface.on_interfaces_added(_interface_added)  # type: ignore
 
-        yielded_paths = set()
+        # Yield any devices which are already present.
+        devs = await self.get_devices()
+        yielded_paths = set(devs)
+
+        for d in devs:
+            yield d
 
         async def _stop_discovery() -> None:
             await asyncio.sleep(duration)
             await self.stop_discovery()
 
         await self.start_discovery()
-        stop_task = None
+        timeout_task = None
         if duration > 0:
-            stop_task = asyncio.create_task(_stop_discovery())
+            timeout_task = asyncio.create_task(_stop_discovery())
 
         adapter_path = self._adapter_interface.path
         while not self._discovery_stopped.is_set():
-            if stop_task is not None:
+            if timeout_task is None:
+                path, intfs_and_props = await queue.get()
+            else:
                 queue_task = asyncio.create_task(queue.get())
 
+                # Block until either a timeout or we find a device.
                 done, _ = await asyncio.wait(
-                    [queue_task, stop_task],
+                    [queue_task, timeout_task],
                     return_when=asyncio.FIRST_COMPLETED,
                 )
 
-                if stop_task in done and queue_task not in done:
+                # Break out if we timed out and didn't find a device.
+                if timeout_task in done and queue_task not in done:
                     queue_task.cancel()
                     try:
                         await queue_task
@@ -245,9 +255,7 @@ class Adapter:
                     break
 
                 path, intfs_and_props = queue_task.result()
-            else:
-                path, intfs_and_props = await queue.get()
-
+                
             if (
                 path.startswith(adapter_path)
                 and path in yielded_paths
@@ -258,10 +266,11 @@ class Adapter:
             yield await self._get_device(path)
             yielded_paths.add(path)
 
-        if stop_task is not None and stop_task.done():
-            stop_task.cancel()
+        # Cancel the timeout task if it's still pending (discovery must have been cancelled by someone else). 
+        if timeout_task is not None and not timeout_task.done():
+            timeout_task.cancel()
             try:
-                await stop_task
+                await timeout_task
             except asyncio.CancelledError:
                 pass
 
